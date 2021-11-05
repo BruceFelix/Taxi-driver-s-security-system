54df786ec1322828b739a28011facd282f5ce03d
Bruce Felix <brucefelixm@gmail.com>
2021-11-05 19:27:13 +0300

    backend for sign up page  done

--- Back-end/connection.php
+++ Back-end/connection.php
@@ -2,8 +2,8 @@
 $user = 'cybernomand';
 $host = "localhost";
 $password = "D4yl!4D?Oef4ISYC";
-
-if($connection = mysqli_connect($host,$user,$password)){
+$database = "Taxi_drivers_security_system";
+if($connection = mysqli_connect($host,$user,$password,$database)){
     // echo "<h3 style='color:green'>Connected successfully</h3>";
 }
 else{
@@ -20,6 +20,18 @@ $createdb = "CREATE DATABASE Taxi_drivers_security_system";
 //     echo "<h3 style='color:red'>DB not created</h3>".mysqli_error($connection);
 
 // }
-$tableCreator = "CREATE TABLE driversdetails (id int auto_increment, name varchar(30), password varchar(253), primary key(id), email varchar(50) , dob DATE, code varchar(50))";
-
+// $tableCreator = "CREATE TABLE driversdetails (id int auto_increment, name varchar(30), primary key(id),phonenumber int(50),carplates varchar(30) ,carmodel varchar(50), password varchar(253))";
+// if(mysqli_query($connection, $tableCreator)){
+//     echo "Table created successfully";
+// }
+// else{
+//     echo "Table not created".mysqli_error($connection);
+// }
+// $dispatchertable = "CREATE TABLE dispatcherdetails (id int auto_increment, name varchar(30), primary key(id), email varchar(30), password varchar(253))";
+// if(mysqli_query($connection, $dispatchertable)){
+//     echo "Table created";
+// }
+// else{
+//     echo "Not created".mysqli_error($connection);
+// }
 ?>
