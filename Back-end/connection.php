8f9a07aedb5dd022b20e8a9753d2ba4c12504a84
Bruce Felix <brucefelixm@gmail.com>
2021-11-10 06:28:18 +0300

    Working on loggin backend

--- Back-end/connection.php
+++ Back-end/connection.php
@@ -20,13 +20,13 @@ $createdb = "CREATE DATABASE Taxi_drivers_security_system";
 //     echo "<h3 style='color:red'>DB not created</h3>".mysqli_error($connection);
 
 // }
-// $tableCreator = "CREATE TABLE driversdetails (id int auto_increment, name varchar(30), primary key(id),phonenumber int(50),carplates varchar(30) ,carmodel varchar(50), password varchar(253))";
-// if(mysqli_query($connection, $tableCreator)){
-//     echo "Table created successfully";
-// }
-// else{
-//     echo "Table not created".mysqli_error($connection);
-// }
+$tableCreator = "CREATE TABLE usersdetails (id int auto_increment, name varchar(30), primary key(id),phonenumber int(50),carplates varchar(30) ,carmodel varchar(50), password varchar(253))";
+if(mysqli_query($connection, $tableCreator)){
+    echo "Table created successfully";
+}
+else{
+    echo "Table not created".mysqli_error($connection);
+}
 // $dispatchertable = "CREATE TABLE dispatcherdetails (id int auto_increment, name varchar(30), primary key(id), email varchar(30), password varchar(253))";
 // if(mysqli_query($connection, $dispatchertable)){
 //     echo "Table created";
